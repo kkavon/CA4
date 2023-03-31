ebea5d43d2e661d2c9fa8dbd4a8215023a49094e
kkavon <61874951+kkavon@users.noreply.github.com>
2023-03-31 16:38:50 +0100

    Fixed unnecessary SQL

--- UserRepository.java
+++ UserRepository.java
@@ -1,12 +1,13 @@
 package net.codejava;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmailAndPassword(String email, String password);
-    User findByEmail(String email);
 
+	   User findByEmailAndPassword(String email, String password);
+	    User findByEmail(String email);
 
 }
